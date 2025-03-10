944c2c910c50e2e04bd88509d239ac202be09e0a
Daniel Ortiz <andresvertel2021@gmail.com>
2025-03-10 10:07:02 -0500

    sidebar completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AuthService } from './core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:
+  [ RouterOutlet,
+    SidebarComponent,
+    CommonModule
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend-solicitud';
+  isAuthenticated = false;
+
+  constructor(private authService: AuthService) {} // Inyecta AuthService
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe(
+      (isAuthenticated) => (this.isAuthenticated = isAuthenticated)
+    );
+  }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
+  links: { path: string, label: string }[] = [
+    { path: '/dashboard', label: 'Dashboard' },
+    { path: '/solicitudes', label: 'Solicitudes' },
+    { path: '/candidatos', label: 'Candidatos' }
+  ];
+
+  constructor(private authService: AuthService, private router: Router) {} // Inyecta AuthService y Router
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']); // Redirige al login después de cerrar sesión
+  }
 }
